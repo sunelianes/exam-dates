1deb69699297f46ce15832f4b81eed008e9e57ab
Sune.Lianes <sune.lianes@student.uib.no>
2024-06-10 14:17:46 +0200

    removed methods in interface and moved them to abstract class

---
+++ src/main/java/org/example/ExamDateFinder.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.Scanner;
+
+public abstract class ExamDateFinder implements IExamDateFinder {
+    String getSubjectCode() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter subject code: ");
+        return scanner.nextLine();
+    }
+    Document connect(String url) {
+        try {return Jsoup.connect(url).get();}
+        catch (Exception e) {
+            return null;
+        }
+    }
+}
--- src/main/java/org/example/IExamDateFinder.java
+++ src/main/java/org/example/IExamDateFinder.java
@@ -6,8 +6,6 @@ import org.jsoup.nodes.Element;
 
 public interface IExamDateFinder {
     String findExamDate();
-    String getSubjectCode();
-    Document connect(String url);
-    String getTitle(Document document);
-    Element getExamElem(Document document);
+    String getTitle();
+    Element getExamElem();
 }
