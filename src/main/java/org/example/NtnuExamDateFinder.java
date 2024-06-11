49564839b5b391c555369eb5a8a9036d50b1e460
Sune.Lianes <sune.lianes@student.uib.no>
2024-06-11 19:45:58 +0200

    implemented easy version for ntnu

---
+++ src/main/java/org/example/NtnuExamDateFinder.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class NtnuExamDateFinder extends ExamDateFinder {
+    private final String baseURL = "https://www.ntnu.no/studier/emner/";
+    private final String endOfURL = "#tab=omEksamen";
+    private final Document doc;
+
+    public NtnuExamDateFinder() {
+        String url = baseURL + getSubjectCode() + endOfURL;
+        this.doc = connect(url);
+    }
+
+    @Override
+    public String getExamDate() {
+        String examDate = findExamElem().text();
+        return examDate.isEmpty() ? "No Exam Date Found" : examDate;
+    }
+
+    @Override
+    public String getTitle() {
+        return doc.title();
+    }
+
+    @Override
+    public Element findExamElem() {
+        try {
+            return doc.select("span.exam-date").first();
+        }
+        catch (NullPointerException e) {
+            System.out.println("error");
+            return new Element("No exam date found");
+        }
+    }
+}
