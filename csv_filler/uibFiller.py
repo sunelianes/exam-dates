cb7c3440ebbf070b49b9284395538c20739e6df8
Sune.Lianes <sune.lianes@student.uib.no>
2024-06-12 16:46:24 +0200

    tried out a short script

--- csv_filler/uibFiller.py
+++ csv_filler/uibFiller.py
@@ -2,6 +2,10 @@
 import csv
 
 with open("uib_exams.csv", mode="w") as exam_file:
-    fieldnames = ['Exam part', 'Year/Sem', 'Date and time', 'Duration', 'Location']
+    fieldnames = ["Subject Code", "Exam part", "Year/Sem", "Date and time", "Duration", "Location"]
     writer = csv.DictWriter(exam_file, fieldnames=fieldnames)
     writer.writeheader()
+    writer.writerow({"Subject Code": "INF112", "Exam part": "Written exam",
+                    "Year/Sem": "24H" , "Date and time": "25.05.2023 15:00",
+                    "Duration": "5 hours", "Location": "Solheimsgaten"})
+
